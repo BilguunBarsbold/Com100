acd9ed0b22527717e48a3065f798457afe61d2e7
BilguunBarsbold <bilguun99859899@gmail.com>
2021-05-18 19:10:31 +0800

    'assignment1'

--- intro.php
+++ intro.php
@@ -3,11 +3,21 @@
     <div class='container'>
         <div class='row'>
             <div class='cols-12'>
-                <h1>Сайн байна уу.</h1>
-                20B1BUM1083 дугаартай Б.Билгүүн байна.
+                <div>
+                    <h1>20B1BUM1083 дугаартай Б.Билгүүн</h1>
+                </div>
+                <div class='text'>
+                    <div>
+                        <img src="family.jpg" class='family' alt="No result">
+                    </div>
+                    <div class='textarea'>
+                        Сайн байна уу, намайг Билгүүн гэдэг. Ам бүл дөрөвүүлээ амьдардаг.
+                    </div>
+                </div>
                 <hr>
                 <a href="index.html">Back home</a>
             </div>
         </div>
     </div>
+    <link rel='stylesheet' href='style.css'>
 <?php include 'includes/footer.php';?>
